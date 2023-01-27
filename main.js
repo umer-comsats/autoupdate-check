cbee0662ddb9960fe826fe68d3af104fb7608e8a
umer-comsats <iumerusman@gmail.com>
2023-01-27 14:32:00 +0500

    changed

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const { autoUpdater, AppUpdater } = require("electron-updater");
 
 let mainWindow;
+
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({
@@ -16,7 +20,8 @@ const createWindow = () => {
   }
 
   app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+    autoUpdater.checkForUpdates();
   })
 
   app.on('window-all-closed', () => {
@@ -26,3 +31,12 @@ const createWindow = () => {
   ipcMain.on("app:getVersion", () => {
     mainWindow.webContents.send("app:setVersion", app.getVersion());
   });
+
+  autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send("app:updateAvailable");
+    autoUpdater.downloadUpdate();
+  });
+
+  autoUpdater.on("update-not-available", () => {
+    mainWindow.webContents.send("app:updateNotAvailable");
+  })
